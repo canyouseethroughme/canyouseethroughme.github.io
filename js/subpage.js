0e12d8f949cdabec72be2a221acf05eb4be0733b
Stefan Andrei <picotto@Stefans-MacBook-Pro.local>
2018-10-17 16:23:55 +0200

    done

--- js/subpage.js
+++ js/subpage.js
@@ -1,11 +1,7 @@
 
 let urlParams = new URLSearchParams(window.location.search);
-
+let template = document.querySelector("section");
 let id = urlParams.get("id");
-console.log("i want to get article: " + id);
-
-
-
 
 fetch("http://canyouseethrough.me/kea2/theme7/wordpress/wp-json/wp/v2/projects/" + id)
     .then(e => e.json())
@@ -18,10 +14,14 @@ function showSinglePost(aPost) {
     document.querySelector(".title2").innerHTML = aPost.title.rendered;
     document.querySelector(".content").innerHTML = aPost.content.rendered;
 
-    // let clone = img.cloneNode(true);
 
-    // if (aPost.acf.my_projects_images.url != "") {//img is there
-    //     clone.querySelector(".project-img").setAttribute("src", aPost.acf.my_projects_images.url);
-    // }
+
+    if (aPost.acf.my_projects_images.url != "") {//img is there
+        document.querySelector(".project-img").setAttribute("src", aPost.acf.my_projects_images.url);
+    }
+
+    if (aPost.acf.urlToProject != "") {
+        document.querySelector(".linkwebsite").setAttribute("href", aPost.acf.urlToProject);
+    }
 }
 
