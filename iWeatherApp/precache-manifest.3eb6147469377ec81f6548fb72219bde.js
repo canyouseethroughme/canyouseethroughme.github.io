dcb4ee14c2f6f4eb306a33e0e75a825cf14fda1c
Stefan Andrei <stefandrei123@gmail.com>
2020-04-27 10:20:17 +0200

    auto-commit

---
+++ iWeatherApp/precache-manifest.3eb6147469377ec81f6548fb72219bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d8bc52f4f4409b831e48783e41e073",
+    "url": "/iWeatherApp/index.html"
+  },
+  {
+    "revision": "ca68c77082b51d029a3c",
+    "url": "/iWeatherApp/static/css/main.37442083.chunk.css"
+  },
+  {
+    "revision": "d1b686798733e015d21a",
+    "url": "/iWeatherApp/static/js/2.38466473.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/iWeatherApp/static/js/2.38466473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca68c77082b51d029a3c",
+    "url": "/iWeatherApp/static/js/main.25744f61.chunk.js"
+  },
+  {
+    "revision": "043830b50982762f6f51",
+    "url": "/iWeatherApp/static/js/runtime-main.1c2bb923.js"
+  }
+]);
